300b803ea3b41ae7686c52bcd317819edacfb47a
Ilya Dryomov <idryomov@gmail.com>
2024-05-24 10:16:01 +0200

    qa/workunits/rbd: run mock tests under valgrind
    Currently librbd and rbd-mirror mock tests are run only as part of
    "make check", not under valgrind.
    
    Fixes: XXX
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- qa/workunits/rbd/test_librbd.sh
+++ qa/workunits/rbd/test_librbd.sh
@@ -1,9 +1,14 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
+
+set -ex
 
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --error-exitcode=42 ceph_test_librbd
+  valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
+    --error-exitcode=42 unittest_librbd
 else
   ceph_test_librbd
 fi
-exit 0
+
+echo OK
--- qa/workunits/rbd/test_rbd_mirror.sh
+++ qa/workunits/rbd/test_rbd_mirror.sh
@@ -1,9 +1,14 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
+
+set -ex
 
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --error-exitcode=42 ceph_test_rbd_mirror
+  valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
+    --error-exitcode=42 unittest_rbd_mirror
 else
   ceph_test_rbd_mirror
 fi
-exit 0
+
+echo OK
